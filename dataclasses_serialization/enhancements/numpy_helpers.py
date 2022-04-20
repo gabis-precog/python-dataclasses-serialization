ea669b5f68dbeb2d3e52f366e056ac167a6fa343
gabi <gabis@precog.co>
2022-04-20 19:39:10 +0300

    commented out numpy test code

--- dataclasses_serialization/enhancements/numpy_helpers.py
+++ dataclasses_serialization/enhancements/numpy_helpers.py
@@ -3,5 +3,5 @@ import numpy
 serializers = {
     numpy.bool_: lambda value: bool(value),
     numpy.int64: lambda value: int(value),
-    numpy.ndarray: lambda lst: list(map(self.serialize, lst)),
+    # numpy.ndarray: lambda lst: list(map(self.serialize, lst)),
 }
