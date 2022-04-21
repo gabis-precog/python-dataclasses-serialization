86fd023b43ee08cf6ee98f9a72584385746dd1ec
gabi <gabis@precog.co>
2022-04-21 18:16:10 +0300

    documentation

--- dataclasses_serialization/__init__.py
+++ dataclasses_serialization/__init__.py
@@ -8,7 +8,6 @@
 Suppose we have the following dataclass:
 
 >>> from dataclasses import dataclass
-
 >>> @dataclass
 ... class InventoryItem:
 ...    name: str
@@ -18,10 +17,14 @@ Suppose we have the following dataclass:
 Then we may serialize/deserialize it to/from JSON by using `dataclasses_serialization.mapper.json_mapper.JsonMapper`
 
 >>> from dataclasses_serialization.mapper.json_mapper import JsonMapper
-
 >>> mapper = JsonMapper()
+
+This can be used to serialize:
+
 >>> mapper.serialize(InventoryItem("Apple", 0.2, 20))
 {'name': 'Apple', 'unit_price': 0.2, 'quantity_on_hand': 20}
+
+and deserialize:
 
 >>> mapper.deserialize(InventoryItem, {'name': 'Apple', 'unit_price': 0.2, 'quantity_on_hand': 20})
 InventoryItem(name='Apple', unit_price=0.2, quantity_on_hand=20)
