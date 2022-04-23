aac09a94f5d1408b38a44c57b2453a09ebccc9a5
gabi <gabis@precog.co>
2022-04-23 22:34:46 +0300

    added attrs extension documentation

--- dataclasses_serialization/extensions/__init__.py
+++ dataclasses_serialization/extensions/__init__.py
@@ -4,6 +4,7 @@ Extensions are available to handle some non-native data types:
  - dateutil
  - key_mapping
  - numpy
+ - attrs
 
 Install using the pip extensions mechanism.
 
@@ -47,4 +48,44 @@ Support for mapping some numpy types. Example usage:
 >>> mapper.deserialize(numpy.ndarray, [[1,2,3],[4,5,6]])
 array([[1, 2, 3],
        [4, 5, 6]])
+
+attrs
+-----
+
+Support for mapping attrs classes which have type hints.
+
+
+>>> from dataclasses_serialization.extensions.attrs_helpers import attrs_serializers, attrs_deserializers
+>>> from dataclasses_serialization.mapper.json_mapper import JsonMapper
+
+Assumeing an attrs supported class is defined:
+
+>>> from typing import Dict
+>>> from attr import define
+>>> @define
+... class SampleAttrs:
+...    number: int
+...    name: str
+...    lookup: Dict[str, int]
+
+Define a mapper with attrs support:
+
+>>> mapper = (JsonMapper()
+...           .register_serializers(attrs_serializers)
+...           .register_deserializers(attrs_deserializers)
+...          )
+
+An instance can be serialized:
+
+>>> item = SampleAttrs(3, 'a', {'b': 2})
+>>> mapper.serialize(item)
+{'number': 3, 'name': 'a', 'lookup': {'b': 2}}
+
+And deserialized:
+
+>>> item = {'number': 3, 'name': 'a', 'lookup': {'b': 2}}
+>>> mapper.deserialize(SampleAttrs, item)
+SampleAttrs(number=3, name='a', lookup={'b': 2})
+
 """
+
